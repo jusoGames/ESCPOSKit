3a4d874714a2b317a0e4966b16ea9f0bca0b5af3
Juan De Robles <39359208+jusoGames@users.noreply.github.com>
2024-02-08 21:36:33 -0600

    Update Cescpos.c

--- Sources/Cescpos/Cescpos.c
+++ Sources/Cescpos/Cescpos.c
@@ -7,7 +7,7 @@
 
 #include "include/Cescpos.h"
 
-int escpos_print(UInt32 idVendor, UInt32 idProduct, const char *buffer, unsigned int length,byte buspipe) {
+int escpos_print(UInt32 idVendor, UInt32 idProduct, byte buspipe,const char *buffer, unsigned int length) {
     
     // Let’s now implement the main function. Add the following variable declarations.
     CFMutableDictionaryRef matchingDictionary = NULL;
